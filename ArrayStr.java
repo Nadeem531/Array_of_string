3be9e4af1433ff8fa06e2c18434d07551ef3068c
Nadeem531 <97834155+Nadeem531@users.noreply.github.com>
2022-01-19 11:49:36 -0800

    Add files via upload

---
+++ ArrayStr.java
@@ -0,0 +1,25 @@
+package arrString;
+
+
+public class ArrayStr {
+	public static void main(String[] args)
+	{
+		String[] arr = { "Apple", "Banana", "Orange" };
+		String key = "Banana";
+		boolean flag = false;
+		
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == key) {
+				System.out.println("found in index"+ i);
+				flag = true;
+			}
+		}
+		if (flag == false) {
+			System.out.println("Not found");
+		}
+	}
+}
+
+	
+
+
